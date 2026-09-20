2604c26d809ce0bc1024694dbfa5609c58dfebe6
Abd-RamaneSENI <seniramane@gmail.com>
2026-09-20 13:27:24 +0100

    Correction de la syntaxe CORS avec la bonne URL Vercel

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,26 @@
 <?php
 
 return [
+
     'paths' => ['api/*', 'webhooks/*', 'livres/*', 'telechargements/*', 'lecture/*', 'commandes/*'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', env('FRONTEND_URL', 'http://localhost:5173'))))),
+
+    'allowed_origins' => [
+        'https://react-frontend-pi-nine.vercel.app',
+        'http://localhost:5173', 
+        'http://127.0.0.1:5173',
+    ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => true,
+
 ];
+
